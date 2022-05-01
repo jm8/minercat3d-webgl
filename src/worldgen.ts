90cd06511f5ce755728f59348e43760179e8dfd0
Josh <jm8hjkl@gmail.com>
2022-04-30 20:25:17 -0400

    more worldgen. todo: caves

--- src/worldgen.ts
+++ src/worldgen.ts
@@ -1,13 +1,14 @@
 import { LAYER_SIZE, Blocks, WORLD_DEPTH, WORLD_SIZE } from "./main"
 
-type WorldGen = { y: number, tries: Try[] }[]
+type WorldGen = WorldGenSection[]
+
+type WorldGenSection = { y: number, tries: Try[], cavechance: number } | ({ y: number, tries: Try[] })
 
 type Try = { chance: number, do: Do } | { do: Do }
 
 type Do =
   | number
   | { type: "firstlayer" }
-  | { type: "cave" }
 
 const worldgen: WorldGen = [
   { y: 5, tries: [{ do: 0 }] },
@@ -21,7 +22,7 @@ const worldgen: WorldGen = [
       { chance: 14, do: 6 },
       { chance: 16, do: 7 },
       { do: 3 },
-    ]
+    ],
   },
   {
     y: 24,
@@ -30,14 +31,14 @@ const worldgen: WorldGen = [
       { chance: 12, do: 5 },
       { chance: 14, do: 6 },
       { chance: 16, do: 7 },
-      { chance: 45, do: {type: "cave"} },
       { do: 3 }
-    ]
+    ],
+    cavechance: 63,
   }
 ]
 
 // inclusive
-const randint = (a: number, b: number) => Math.floor(Math.random() * (a + 1 - b) + a)
+const randint = (a: number, b: number) => Math.floor(Math.random() * (b + 1 - a) + a)
 
 export function generate(blocks: Blocks) {
   let section = 0;
@@ -48,35 +49,50 @@ export function generate(blocks: Blocks) {
       for (let z = 0; z < WORLD_SIZE; z++) {
         for (const tri of worldgen[section].tries) {
           if ("chance" in tri) {
-            // console.log(Math.random(), 1/ tri.chance)
             if (Math.random() >= 1 / tri.chance) continue;
           }
 
           if (typeof tri.do == "number") {
             blocks.setBlock([x, y, z], tri.do);
           } else if (tri.do.type == "firstlayer") {
-            blocks.setBlock([x, y, z], 1);
-          } else if (tri.do.type == "cave") {
-            cave(blocks, x, y, z)
+            const center = Math.floor(WORLD_SIZE / 2);
+            if ((x == center || x == center - 1) && (z == center || z == center - 1)) {
+              blocks.setBlock([x, y, z], 41);
+            }
+            else {
+              blocks.setBlock([x, y, z], 1);
+            }
           }
-
           break;
+        }
+      }
+    }
+    const thesection = worldgen[section];
+    if ("cavechance" in thesection) {
+      for (let x = 0; x < WORLD_SIZE; x++) {
+        for (let z = 0; z < WORLD_SIZE; z++) {
+          if (Math.random() < 1 / thesection.cavechance) {
+            cave(blocks, x, y, z);
+          }
         }
       }
     }
   }
 }
 
+
 function cave(blocks: Blocks, x: number, y: number, z: number) {
   const width = randint(3, 7);
   const depth = randint(3, 7);
-  
+
+  console.log(width, depth);
+
   for (let dx = 0; dx < width; dx++) {
     for (let dz = 0; dz < depth; dz++) {
       for (let dy = 0; dy < 3; dy++) {
-        blocks.setBlock([x+dz, y+dy, z+dz], 0);
+        blocks.setBlock([x + dz, y - dy, z + dz], 0);
       }
     }
   }
-  
+
 }
